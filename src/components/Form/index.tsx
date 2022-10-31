cebe1f9431b662aa775d3dbd4fe00cc19c9f3681
Paulo Moutinho <paulo_vicali@icloud.com>
2022-10-31 20:16:20 -0300

    main file

---
+++ src/components/Form/index.tsx
@@ -0,0 +1,27 @@
+import { FormControl, FormLabel, Input as ChakraInput, InputProps as ChakraInputProps } from "@chakra-ui/react";
+
+type InputProps = {
+    name: string
+    label?: string
+} 
+
+type TotalProps = InputProps & ChakraInputProps // intersecção de tipos
+
+export function Input({ name, label, ...rest }: TotalProps) {
+    return (
+        <FormControl>
+          {label ? <FormLabel htmlFor={name}>{label}</FormLabel> : ''}
+          <ChakraInput
+            type={"email"}
+            name={name}
+            id={name}
+            focusBorderColor="teal.400"
+            bg={"gray.900"}
+            variant={"filled"}
+            _hover={{ bg: "gray.900" }}
+            size={"lg"}
+            {...rest}
+          />
+        </FormControl>
+    )
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,45 @@
+import { Button, Flex } from "@chakra-ui/react";
+import { Input } from "../components/Form";
+
+export default function SignIn() {
+  return (
+    <Flex
+      alignItems={"center"}
+      justifyContent={"center"}
+      h={"100vh"}
+      w={"100vw"}
+      px={"2rem"} // responsividade
+    >
+      <Flex
+        as={"form"}
+        w={"100%"}
+        maxWidth={"23rem"} // 360px
+        bg={"gray.800"}
+        p={"2rem"} // 32px
+        borderRadius={"0.5rem"} //8px
+        flexDirection={"column"}
+        gap={"1rem"} // 16px
+      >
+        <Input 
+          name={"email"} 
+          type={"email"} 
+          label={"E-mail"} 
+        />
+        <Input 
+          name={"password"} 
+          type={"password"} 
+          label={"Senha"} 
+        />
+
+        <Button
+          type={"submit"}
+          mt={"1.5rem"}
+          bg={"teal.400"}
+          _hover={{ bg: "teal.600" }}
+        >
+          Entrar
+        </Button>
+      </Flex>
+    </Flex>
+  );
+}
