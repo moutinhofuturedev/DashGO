faa17048ae3fff4cfc1b8c1caf8ad43944995855
Paulo Moutinho <paulo_vicali@icloud.com>
2022-11-02 16:40:37 -0300

    title description

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-title-in-document-head */
 import Document, { Head, Html, Main, NextScript } from "next/document"
 
 export default class MyDocument extends Document {
@@ -5,8 +6,10 @@ export default class MyDocument extends Document {
         return(
             <Html>
                 <Head>
+                    <link rel="icon" type="image/svg+xml" href="../../public/dash.svg" />
                     <link rel="preconnect" href="https://fonts.googleapis.com" />
                     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet" />
+                    <title>DashGo</title>
                 </Head>
                 <body>
                     <Main />
