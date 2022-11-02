2c818f659170a5822b402847296e663e73deae1a
Paulo Moutinho <paulo_vicali@icloud.com>
2022-11-02 14:54:32 -0300

    more components: Sidebar

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,7 +1,40 @@
-import { Header } from "../components/Header";
+import { Box, Flex, SimpleGrid, Text, theme } from "@chakra-ui/react"
+import { Header } from "../components/Header"
+import { Sidebar } from "../components/Sidebar"
+import dynamic from "next/dynamic"
+import { series, options } from "../../utils/configs"
+
+const Chart = dynamic(() => import("react-apexcharts"), {
+    ssr: false // Server Sider Rendering está desligado
+})
 
 export default function Dashboard() {
-    return(
+    return (
+      <Flex h={"100vh"} flexDirection={"column"}>
         <Header />
+        <Flex
+          w={"100%"}
+          my={"1.5rem"}
+          maxWidth={1480}
+          marginX={"auto"}
+          px={"1.5rem"}
+        >
+          <Sidebar />
+          <SimpleGrid flex={"0.25rem"} gap={"1rem"} minChildWidth={"20rem"}>
+            <Box p={"2rem"} bg={"gray.800"} borderRadius={"1.5rem"}>
+              <Text fontSize={"lg"} mb={"1rem"}>
+                Inscritos da semana
+              </Text>
+              <Chart type="area" height={160} options={options} series={series}/>
+            </Box>
+            <Box p={"2rem"} bg={"gray.800"} borderRadius={"1.5rem"}>
+              <Text fontSize={"lg"} mb={"1rem"}>
+                Taxa de abertura
+              </Text>
+              <Chart type="area" height={160} options={options} series={series}/>
+            </Box>
+          </SimpleGrid>
+        </Flex>
+      </Flex>
     )
 }
