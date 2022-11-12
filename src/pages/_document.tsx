dc4dcbbc5d290343055f666aa297648cb4b19f8f
Paulo Moutinho <paulo_vicali@icloud.com>
2022-11-12 17:46:12 -0300

    removed title

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,7 +9,6 @@ export default class MyDocument extends Document {
                     <link rel="icon" type="image/svg+xml" href="../../public/dash.svg" />
                     <link rel="preconnect" href="https://fonts.googleapis.com" />
                     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet" />
-                    <title>DashGo</title>
                 </Head>
                 <body>
                     <Main />
