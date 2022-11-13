5075e079b746a5b3905cc40f30e14a7255eae538
Paulo Moutinho <paulo_vicali@icloud.com>
2022-11-13 12:01:55 -0300

    correction

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,6 +1,6 @@
 import { Box, Icon, Link, Stack, Text } from "@chakra-ui/react";
 import { RiContactsLine, RiDashboardLine, RiGitMergeLine, RiInputMethodLine } from "react-icons/ri";
-import { NavLink } from "./Navlink";
+import { NavLink } from "./NavLink";
 import { NavSection } from "./NavSection";
 
 export function Sidebar() {
