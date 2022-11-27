d63a28edb156fe7be8e691c1d5ca1e851f0b02bb
Paulo Moutinho <paulo_vicali@icloud.com>
2022-11-27 13:27:11 -0300

    feat: pasta do create context da sidebar provider

---
+++ src/context/SidebarDrawerContext.tsx
@@ -0,0 +1,27 @@
+import { useDisclosure, UseDisclosureReturn } from "@chakra-ui/react";
+import { useRouter } from "next/router";
+import { createContext, ReactNode, useContext, useEffect } from "react";
+
+type SidebarDrawerProviderProps = {
+    children: ReactNode
+}
+
+type SidebarDrawerContextData = UseDisclosureReturn
+
+const SidebarDrawerContext = createContext({} as SidebarDrawerContextData)
+
+export function SidebarDrawerProvider({ children }: SidebarDrawerProviderProps) {
+    const disclosure = useDisclosure()
+    const router = useRouter()
+
+    useEffect(() => {
+        disclosure.onClose()
+    }, [disclosure, router.asPath])
+    return(
+        <SidebarDrawerContext.Provider value={disclosure}>
+            {children}
+        </SidebarDrawerContext.Provider>
+    )
+}
+
+export const useSidedarDrawer = () => useContext(SidebarDrawerContext)
