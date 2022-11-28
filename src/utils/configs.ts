7f1dc20390d6f2fabe3a1c13b7914ce775fd8853
Paulo Moutinho <paulo_vicali@icloud.com>
2022-11-28 20:40:21 -0300

    feat: adiciona opacidade no gráfico

--- src/utils/configs.ts
+++ src/utils/configs.ts
@@ -7,41 +7,50 @@ export const series = [
 ]
 
 export const options = {
-    chart: {
-        toolbar: {
-            show: false
-        },
-        zoom: {
-            enabled: false
-        },
-        foreColor: theme.colors.gray[500]
+  chart: {
+    toolbar: {
+      show: false,
     },
-    grid: {
-        show: false
+    zoom: {
+      enabled: false,
     },
-    dataLabels: {
-        enabled: false
+    foreColor: theme.colors.gray[500],
+  },
+  grid: {
+    show: false,
+  },
+  dataLabels: {
+    enabled: false,
+  },
+  tooltip: {
+    enabled: false,
+  },
+  xaxis: {
+    type: "datetime",
+    axisBorder: {
+      color: theme.colors.gray[600],
     },
-    tooltip: {
-        enabled: false
+    axisTicks: {
+      color: theme.colors.gray[600],
     },
-    xaxis: {
-        type: "datetime",
-        axisBorder: {
-            color: theme.colors.gray[600]
-        },
-        axisTicks: {
-            color: theme.colors.gray[600]
-        },
-        categories: [
-            "2021-03-18T00:00:00.000Z",
-            "2021-03-19T00:00:00.000Z",
-            "2021-03-20T00:00:00.000Z",
-            "2021-03-21T00:00:00.000Z",
-            "2021-03-22T00:00:00.000Z",
-            "2021-03-23T00:00:00.000Z",
-            "2021-03-24T00:00:00.000Z",
-            "2021-03-25T00:00:00.000Z",
-        ],    
-    }
-}
+    categories: [
+      "2021-03-18T00:00:00.000Z",
+      "2021-03-19T00:00:00.000Z",
+      "2021-03-20T00:00:00.000Z",
+      "2021-03-21T00:00:00.000Z",
+      "2021-03-22T00:00:00.000Z",
+      "2021-03-23T00:00:00.000Z",
+      "2021-03-24T00:00:00.000Z",
+      "2021-03-25T00:00:00.000Z",
+    ],
+    fill: {
+      opacity: 0.3,
+      type: "gradient",
+      gradient: {
+        shade: "dark",
+        opacityFrom: 0.7,
+        opacityTo: 0.3,
+      },
+    },
+  },
+};
