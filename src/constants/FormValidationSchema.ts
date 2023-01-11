47582bac71349b88964cffc66162709ac69e185a
Paulo Moutinho <paulo_vicali@icloud.com>
2023-01-11 20:45:42 -0300

    feat: validando números de caracteres em password

--- src/constants/FormValidationSchema.ts
+++ src/constants/FormValidationSchema.ts
@@ -1,6 +1,6 @@
 import * as yup from "yup"
 
 export const signInFormSchema = yup.object().shape({
-    email: yup.string().required("E-mail é obrigatório").email("E-mail inválido"),
-    password: yup.string().required("Senha é obrigatório")
+    email: yup.string().required("E-mail é obrigatório.").email("E-mail inválido."),
+    password: yup.string().required("Senha é obrigatório.").min(6, "Senha deve conter 6 caracteres ou mais.")
 })
