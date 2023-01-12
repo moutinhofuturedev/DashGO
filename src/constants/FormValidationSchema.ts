1a526c6d8a9924928b59cdcfb27ecd48164334f8
Paulo Moutinho <paulo_vicali@icloud.com>
2023-01-11 21:59:39 -0300

    feat: schema de validação formulário de criação

--- src/constants/FormValidationSchema.ts
+++ src/constants/FormValidationSchema.ts
@@ -4,3 +4,12 @@ export const signInFormSchema = yup.object().shape({
     email: yup.string().required("E-mail é obrigatório.").email("E-mail inválido."),
     password: yup.string().required("Senha é obrigatório.").min(6, "Senha deve conter 6 caracteres ou mais.")
 })
+
+export const userFormSchema = yup.object().shape({
+    name: yup.string().required("Nome é obrigatório"),
+    email: yup.string().required("E-mail é obrigatório.").email("E-mail inválido."),
+    password: yup.string().required("Senha é obrigatório.").min(6, "Senha deve conter 6 caracteres ou mais."),
+    password_confirmation: yup.string().oneOf([
+        null, yup.ref("password")
+    ], "As senhas precisam ser iguais")
+})
